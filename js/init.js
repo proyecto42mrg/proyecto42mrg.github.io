e4a03e37e287dd895138d70b9fd1204ac7a51f2d
proyecto42mrg <82479335+proyecto42mrg@users.noreply.github.com>
2021-04-13 18:50:20 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDdGYQ9gmg86rZjLkueXBxUcal7-xNRdLY",
+    authDomain: "proyecto42am.firebaseapp.com",
+    projectId: "proyecto42am",
+    storageBucket: "proyecto42am.appspot.com",
+    messagingSenderId: "170692477053",
+    appId: "1:170692477053:web:3bac7c1cc6fb68e31e7114",
+    measurementId: "G-TN5MESCBZ2"
+});
